b3a7da4db5300b35630f73ebd4dd4279409235c8
daniil_stepanenko <daniil_stepanenko@epam.com>
2021-10-18 16:35:43 +0300

    use postgres.Colomn insted postgres.ColumnInteger

--- pkg/interfaces/repository/jet/query_utils.go
+++ pkg/interfaces/repository/jet/query_utils.go
@@ -2,6 +2,6 @@ package jet
 
 import "github.com/go-jet/jet/v2/postgres"
 
-func asIs(col postgres.ColumnInteger) postgres.Projection {
+func asIs(col postgres.Column) postgres.Projection {
 	return col.AS(col.Name())
 }
